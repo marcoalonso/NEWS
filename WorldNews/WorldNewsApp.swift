a3bee2a3382e5fb8fcbe32c2dd618b6ea3ae8bc4
Marco Alonso <mrodriguez@alquimiapay.com>
2022-08-24 10:00:06 -0500

    Getting started

---
+++ WorldNews/WorldNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorldNewsApp.swift
+//  WorldNews
+//
+//  Created by marco rodriguez on 24/08/22.
+//
+
+import SwiftUI
+
+@main
+struct WorldNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
