516f9d398d225b40fa7a41b21f59feea74ec056a
Marco Alonso <mrodriguez@alquimiapay.com>
2022-08-24 22:21:34 -0500

    API singleton Instance Added

---
+++ WorldNews/API/NewsAPI.swift
@@ -0,0 +1,57 @@
+//
+//  NewsAPI.swift
+//  WorldNews
+//
+//  Created by marco rodriguez on 24/08/22.
+// https://www.youtube.com/watch?v=LRnbFjq0bTM continue at 54:43 
+ 
+import Foundation
+
+struct NewsAPI {
+    static let shared = NewsAPI()
+    private init() { }
+    
+    private let apiKey = "f0797ef3b62d4b90a400ed224e0f82b7"
+    private let session = URLSession.shared
+    private let jsonDecoder: JSONDecoder = {
+        let decoder = JSONDecoder()
+        decoder.dateDecodingStrategy = .iso8601
+        return decoder
+    }()
+    
+    func fetch(from category: Category) async throws -> [Article] {
+        let url = generateNewsURL(from: category)
+        let (data, response) = try await session.data(from: url)
+        
+        guard let response = response as? HTTPURLResponse else {
+            throw generateError(description: "Bad Response")
+        }
+        
+        switch response.statusCode {
+        
+        case (200...299), (400...499):
+            let apiResponse = try jsonDecoder.decode(NewsAPIResponse.self, from: data)
+            if apiResponse.status == "ok" {
+                return apiResponse.articles ?? []
+            } else {
+                throw generateError(description: apiResponse.message ?? "An error occured")
+            }
+            
+        default:
+            throw generateError(description: "An server error occured")
+        }
+    }
+        private func generateError(code: Int = 1, description: String) -> Error {
+            NSError(domain: "NewsAPI", code: code, userInfo: [NSLocalizedDescriptionKey: description])
+        }
+        
+    
+    private func generateNewsURL(from category: Category) -> URL {
+        var url = "https://newsapi.org/v2/top-headlines?"
+        url += "apiKey=\(apiKey)"
+        url += "&language=es"
+        url += "&category=\(category.rawValue)"
+        return URL(string: url)!
+    }
+    
+}
